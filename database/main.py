5eeb3b440bc5bc87a571a26b2d5bedbb2c79bdcd
Aljes Binkevich <binkevich.as@phystech.edu>
2025-01-09 19:30:56 +0000

    Added a separate file to fill the database

---
+++ database/main.py
@@ -0,0 +1,45 @@
+from models import Base, engine, Exercise
+from re import Match
+from typing import Dict
+from sqlalchemy.orm import Session
+from parsers import parse_paragraphs, parse_exercises, get_exercise_data
+import config
+
+
+def save_exercise(exercise_data: Dict[str, str]) -> None:
+    """
+    Save exercise to the database
+    Args:
+        exercise_data (Dict[str, str]): exercise data
+    """
+    exercise = Exercise(**exercise_data)
+    with Session(bind=engine) as session:
+        session.add(exercise)
+
+
+def process_paragraphs(filepath: str) -> None:
+    """
+    Parse paragraphs from a file and save exercises to the database
+    Args:
+        filepath (str): file path to the book in markdown extension
+    """
+    # Read the book
+    with open(filepath, "r") as file:
+        text = file.read()
+
+    # Parse the book paragraph by paragraph and add exercises to the DataBase
+    for paragraph_match in parse_paragraphs(text):
+        # parse exercise section of the book
+        exercise_section = paragraph_match.group("exercises") or ""
+        for exercise_match in parse_exercises(exercise_section):
+            exercise = get_exercise_data(paragraph_match, exercise_match)
+            save_exercise(exercise)
+
+
+def main():
+    Base.metadata.create_all(engine)
+    process_paragraphs(config.BOOK_FILEPATH)
+
+
+if __name__ == "__main__":
+    main()
