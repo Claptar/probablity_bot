962fc993377002b2407fe32b1a3317fa8845636f
Aljes Binkevich <binkevich.as@phystech.edu>
2025-01-18 20:31:47 +0000

    added section parser

---
+++ app/parsers/section.py
@@ -0,0 +1,24 @@
+import re
+from typing import Iterator
+
+
+SECTION_PATTERN = re.compile(
+    r"""
+    ^\#\#\s+Chapter\s(?P<number>\d{1,2})$  # Section number
+    \n+                                 # Required empty lines
+    ^\#\#\s+(?P<contents>[\w ]+)$        # Section title
+    """,
+    re.VERBOSE | re.MULTILINE,
+)
+
+
+def parse_section(text: str) -> Iterator[re.Match]:
+    """
+    Parse sections from a solution mannual
+    Args:
+        text (str): solution mannual's text in markdown format
+
+    Returns:
+        Iterator[re.Match]: iterator of section matches
+    """
+    return SECTION_PATTERN.finditer(text)
