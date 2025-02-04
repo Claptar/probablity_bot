35994d85927dd681fbe18ad7fb11dc7d6de15904
Aljes Binkevich <binkevich.as@phystech.edu>
2025-02-04 22:28:39 +0000

    Added exercise contents validation

--- app/database/models/exercises.py
+++ app/database/models/exercises.py
@@ -1,11 +1,12 @@
 "Contains the Exercise class that represents an exercise from the book, stored in the database"
+import logging
+from typing import Dict, Type
 from app.database.models.base import CommonAttributes
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, ForeignKey
 from app.database.models.paragraphs import Paragraph
 from app.database.models.solutions import Solution
 from app.database.models.sections import Section
-from typing import Dict, Type
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy import Column, Integer, ForeignKey
 
 
 class Exercise(CommonAttributes):
@@ -16,16 +17,33 @@ class Exercise(CommonAttributes):
     __tablename__ = "exercises"
 
     solution_id = Column(
-        Integer, ForeignKey("solutions.id"), nullable=True, unique=True
+        Integer, ForeignKey("solutions.id"), nullable=False, unique=True
     )
     score = Column(Integer, default=1)
-    
+
     paragraph = relationship("Paragraph", back_populates="exercise", uselist=False)
     solution = relationship("Solution", back_populates="exercise", uselist=False)
     solved_exercises = relationship("SolvedExercise", back_populates="exercise")
 
     def __repr__(self) -> str:
         return f"Exercise(number={self.number}, paragraph={self.paragraph}"
+
+    def check_references(self) -> bool:
+        """
+        Check if there are any references in the exercise contents
+        Args:
+            session (Session): database session
+        Returns:
+            bool: True if the contents are valid, False otherwise
+        """
+        keywords = ["Exercise", "Theorem", "Example"]
+        return any(keyword in self.contents for keyword in keywords)
+
+    def refactor_contets(self) -> None:
+        """
+        Refactor exercise contents
+        """
+        self.contents = self.contents.replace("#", "")
 
     @classmethod
     def create(
@@ -55,10 +73,33 @@ class Exercise(CommonAttributes):
             .filter_by(paragraph_id=paragraph.id, number=exercise_data["number"])
             .one_or_none()
         )
+
+        # check if the solution exists in the database
+        if solution is None:
+            logging.error(
+                "Solution for exercise %s not found in the database",
+                exercise_data["number"],
+            )
+            return None
+
+        # create exercise object
         exercise = cls(
             paragraph_id=paragraph.id,
-            solution_id=solution.id if solution else None,
+            solution_id=solution.id,
             **exercise_data,
         )
-        session.add(exercise)
-        return exercise
+
+        # refactor the contents
+        exercise.refactor_contets()
+
+        # check if there are references in the exercise contents
+        if exercise.check_references():
+            logging.info("Creating a new exercise with data: %s", exercise_data)
+            session.add(exercise)
+            return exercise
+        else:
+            logging.error(
+                "Exercise %s contents are invalid. Skipping the exercise",
+                exercise_data["number"],
+            )
+            return None
