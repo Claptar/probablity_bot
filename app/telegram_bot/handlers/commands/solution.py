824c4b41f9c425b9eb818703fc75c06e5258789d
Aljes <aleksandr.binkevich@gmail.com>
2025-02-17 21:49:29 +0000

    Handle exceptions in solution command and improve user feedback for missing solutions

--- app/telegram_bot/handlers/commands/solution.py
+++ app/telegram_bot/handlers/commands/solution.py
@@ -5,6 +5,7 @@ import tempfile
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
+from sqlalchemy.exc import NoResultFound
 from app.database.quieries.queries import user_exercise_soluiton
 from app.utils import latex_to_png
 
@@ -23,7 +24,36 @@ async def solution_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_chat_action("typing")
 
     # Get the solution of the last exercise that the user tried
-    solution_text = user_exercise_soluiton(update.effective_user.id)
+    try:
+        solution_text, exercise_id = user_exercise_soluiton(update.effective_user.id)
+        return await send_solution(update, solution_text, exercise_id)
+    except NoResultFound as e:
+        logging.error("Error while getting the solution of the last exercise: %s", e)
+        message = (
+            "To grasp the mystery you have to go through trial. Take the /challenge"
+        )
+        await update.message.reply_text(message)
+        return "TRIAL"
+    except Exception as e:
+        logging.error(
+            "Error while getting the solution exercise %s: %s", exercise_id, e
+        )
+        message = f"I'm unable to grasp the solution for #trial{exercise_id} at the moment. Try again later."
+        await update.message.reply_text(message)
+        return "TRIAL"
+
+
+async def send_solution(update, solution_text, exercise_id) -> str:
+    """
+    Send the solution to the user
+    Args:
+        update (Update): Telegram update object
+        solution_text (str): The solution text
+        exercise_id (int): The exercise ID
+    Returns:
+        str: The state identifier ("SOLUTION") used to guide the conversation flow
+    """
+    solution_text, exercise_id = user_exercise_soluiton(update.effective_user.id)
 
     # Render exercise image
     logging.info("Rendering LaTeX to PNG for solution: %s", solution_text)
@@ -43,7 +73,9 @@ async def solution_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         ),
     )
     await update.message.reply_chat_action("upload_photo")
-    await update.message.reply_photo(photo=image_path)
+    await update.message.reply_photo(
+        photo=image_path, caption=f"f#solution#trial{exercise_id}"
+    )
 
     # Remove the image
     os.remove(image_path)
