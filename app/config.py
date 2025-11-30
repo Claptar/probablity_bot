ec0213947cd95ec52f46bd86d49805bb328b8d79
Aljes <aleksandr.binkevich@gmail.com>
2025-11-30 20:07:15 +0000

    Update configuration file to load environment variables for additional settings

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
 "Configuration file for the app"
+
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DB_URL")
 BOOK_FILEPATH = os.getenv("BOOK_FILEPATH")
-SOLUTIONS_FILEPATH = os.getenv("SOLUTIONS_FILEPATH")
+SOLUTION_MANNUAL_FILE = os.getenv("SOLUTION_MANNUAL_FILE")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+SECTION_LIST = os.getenv("SECTION_LIST")
+SUBSECTION_FILES_DIR = os.getenv("SUBSECTION_FILES_DIR")
+ELEMENT_TYPES_LIST = os.getenv("ELEMENT_TYPES_LIST")
